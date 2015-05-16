08b6906df432cee5130e8916ded80b54c231ef61
diamant1991 <diamant1988@mail.ru>
2015-05-16 21:23:01 +0300

    first files

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(window).load(function() {
+        $('#slider').nivoSlider({
+           effect: 'fade',
+           pauseTime: 8000,
+           pauseOnHover: false,    
+            })
+        
+    });
