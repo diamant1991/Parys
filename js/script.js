7227a7a3877325bbfd0628ed81f4b719f85ba468
diamant1991 <diamant1988@mail.ru>
2015-05-17 20:58:42 +0300

    add files

--- js/script.js
+++ js/script.js
@@ -31,9 +31,46 @@ $( document ).ready(function() {
       e.preventDefault();
    });
 
-    $('.nav ul li a').click(function(e) {
+    /*$('.nav ul li a').click(function(e) {
         e.preventDefault();
         $('.nav ul li a').removeClass('active');
         $(this).addClass('active');
+    });*/
+
+    $(".work-block").hover(
+      function () {
+        $(this).children('.work-table').addClass('active');
+        $(this).prevAll().children('.work-table').addClass('active');
+      },
+      function () {
+        $(this).children('.work-table').removeClass('active');
+        $(this).prevAll().children('.work-table').removeClass('active');
+      }
+    );
+
+
+    var headerHeight = $('.nav').height();
+  
+  $(".nav a").click(function(e) {
+    e.preventDefault();
+    var offsetTop = $("#"+$(this).data("target")).offset().top - headerHeight;
+    $('html, body').animate({scrollTop: offsetTop}, 1000);
+  });
+ 
+  $(document).scroll(function(){
+    var offsets = [];
+    $('.nav a').each(function(index, element){   
+      offsets.push($("#" + $(element).data('target')).offset().top);
     });
+    
+    offsets.push($(document).height());
+    var docScroll = $(document).scrollTop() + headerHeight + $(window).height() / 2;
+    for (var i = 0; i < offsets.length - 1; i++) {
+      if (docScroll >= offsets[i] && docScroll < offsets[i+1]) {
+        $('.active').removeClass('active');
+        $('.nav a').eq(i).addClass('active');
+        break;
+      };
+    };
+  });
 });
