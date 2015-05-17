8e20f802c56902ef4571f9569a31b06011839bb3
diamant1991 <diamant1988@mail.ru>
2015-05-17 18:09:11 +0300

    add files

--- js/script.js
+++ js/script.js
@@ -6,3 +6,19 @@ $(window).load(function() {
             })
         
     });
+
+$( document ).ready(function() {
+    $('a[href*=#]').bind("click", function(e){
+      var anchor = $(this);
+      $('html, body').stop().animate({
+         scrollTop: $(anchor.attr('href')).offset().top-40
+      }, 1200);
+      e.preventDefault();
+   });
+
+    $('.nav ul li a').click(function(e) {
+        e.preventDefault();
+        $('.nav ul li a').removeClass('active');
+        $(this).addClass('active');
+    });
+});
